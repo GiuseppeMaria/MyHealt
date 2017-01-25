49ab54ede6a65fb443d584bd5042a3c649a6ad03
FrancescoCamposeo <camposeofrancesco1@gmail.com>
2017-01-25 13:10:26 +0100

    first running

--- app/src/main/java/android/vm/myhealt/MainActivity.java
+++ app/src/main/java/android/vm/myhealt/MainActivity.java
@@ -2,6 +2,10 @@ package android.vm.myhealt;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by Francesco on 25/01/2017.
@@ -9,9 +13,25 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+    TextView welcomeTv;
+    Button changeTextBtn;
+    EditText changeTextEdit;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainactivity);
+        welcomeTv = (TextView) findViewById(R.id.welcome_tv);
+        changeTextBtn = (Button) findViewById(R.id.change_text_btn);
+        changeTextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                welcomeTv.setText(changeTextEdit.getText());
+            }
+        });
+        changeTextEdit=(EditText) findViewById(R.id.change_text_edit);
+
+        }
     }
-}
+
